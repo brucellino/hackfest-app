5ad586ce26dcdad4d1885b172a60fe5220d9da30
Bruce Becker <brucellino@gmail.com>
2017-11-19 13:01:12 +0100

    Add default route test

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -8,4 +8,10 @@ class PagesControllerTest < ActionDispatch::IntegrationTest
       get '/pages/home'
       assert_response :success
   end
+
+  # A default route should be specified
+  test "should_get_root" do
+    get '/'
+    assert_response :success
+  end
 end
