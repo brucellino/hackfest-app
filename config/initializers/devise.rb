212fc25bb254e865daf26cd98be12de0a7b4eb07
Bruce Becker <brucellino@gmail.com>
2017-11-20 07:30:48 +0100

    Add sign_in and sign_out devise routes

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -251,6 +251,13 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  User.omniauth_providers.each do |provider_name|
+    api_key = ENV["#{provider_name.upcase}_API_KEY"]
+    api_secret = ENV["#{provider_name.upcase}_API_SECRET"]
+    config.omniauth provider_name, api_key, api_secret
+  end
+
+
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
