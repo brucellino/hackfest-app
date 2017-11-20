dec5e5e3face3387ea2bcc8a757497e633b484ca
Bruce Becker <brucellino@gmail.com>
2017-11-20 07:31:03 +0100

    Change the migration to remove database_trackable

--- db/migrate/20171119193745_devise_create_users.rb
+++ db/migrate/20171119193745_devise_create_users.rb
@@ -1,10 +1,10 @@
 class DeviseCreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
-      ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
-
+       ## OmniAuth-able
+       t.string :provider
+       t.string :uid
+ 
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
@@ -35,7 +35,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.1]
     end
 
     add_index :users, :email,                unique: true
-    add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
