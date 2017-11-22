0dc502b50e1e741558ffa88ea9f3ee1700faf149
Bruce Becker <brucellino@gmail.com>
2017-11-22 07:14:16 +0100

    working auth with github

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+    def self.find_or_create_from_auth_hash(auth)
+        # hash schema is here  : https://github.com/omniauth/omniauth/wiki/Auth-Hash-Schema
+		where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
+			user.provider = auth.provider
+			user.uid = auth.uid
+			user.first_name = auth.extra.raw_infofirst_name
+			user.last_name = auth.extra.raw_info.last_name
+			user.email = auth.extra.raw_info.email
+			user.save!
+		end
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
   get 'pages/:page' => "pages#show"
 
   root 'pages#home', page: "home"
+
+  get 'login', to: redirect('/auth/github'), as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+  get 'home', to: 'home#show'
+  get 'me', to: 'me#show', as: 'me'
+
 end
