d6231bd857e2bdb758398f82d0b8b4e326300e42
Bruce Becker <brucellino@gmail.com>
2017-11-22 06:39:39 +0100

    Change to variable

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, "GOOGLE_CLIENT_ID", "GOOGLE_CLIENT_SECRET"
+  provider :github, ENV['github_api_key'], ENV['github_api_secret']
 end
